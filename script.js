69ba6bca84b446fe4b2f5e420d7031b797baaead
Jan_Niek-PC <yannick.eleveld@coderclass.nl>
2026-03-29 21:26:03 +0200

    heel veel gedoe met links van headers

--- script.js
+++ script.js
@@ -17,29 +17,42 @@ let dumbAgeThingAnimInterval = window.setInterval(() => {
 
 
 //it shows as flex not as block
-function hideShowArticle(/** @type {Element} */ article) {
-    console.log('clicked')
+function hideShowArticle(/** @type {HTMLElement} */ article) {
     for (let child of article.children) {
         if (child.tagName != 'H1'){
             if (child.style.display == 'none'){
                 child.style.display = 'flex'
+                article.style.cursor = 'pointer' //could have different cursor depending on state of article (collapsed/visible)
             }else{
                 child.style.display = 'none'
+                article.style.cursor = 'pointer'
             }
         }
     }
 }
 
-let hidyArticles = document.getElementsByClassName("hideableChildrenNotHeader")
-for (let article of hidyArticles) {
-    article.addEventListener("click", function(){hideShowArticle(article)}) // i love js man
-
-
-    if (!article.classList.contains("startUnhidden")){
-        for (let child of article.children) {
+function hideArticle(/** @type {HTMLElement} */ article){
+    for (let child of article.children) {
             if (child.tagName != 'H1'){
                 child.style.display = 'none'
+                article.style.cursor = 'pointer'
             }
         }
+}
+
+let hidyArticles = document.getElementsByClassName("hideableChildrenNotHeader")
+for (let article of hidyArticles) {
+    article.addEventListener("click", ()=>{hideShowArticle(article)}) // i love js man
+    // console.log(article.id.length)
+
+    // verstopt alles behalve de gelinkte pagina als er een gelinkte pagina is, anders verstopt het alles behalve de elementen met startUnhidden
+    // hides everything except the linked to section if there is a linked to section, otherwise hides everything except elements with the startUnhidden class
+    if((window.location.href.endsWith(article.id) && article.id.length > 1)){
+        for (let article2 of hidyArticles) {
+            if(article != article2) hideArticle(article2)
+        }
+    } else if(!article.classList.contains("startUnhidden")){
+        hideArticle(article)
     }
+    
 }
